407f9a460b7c5dfa9d43a85b2b82b1fce5fc861c
Cioscos <claudiocicca1998@gmail.com>
2024-09-20 16:51:57 +0200

    Fix

--- src/util.py
+++ src/util.py
@@ -187,7 +187,7 @@ def generate_abilities_list_keyboard(abilities: List[Ability],
     keyboard = []
     row = []
     for ability in abilities:
-        button = InlineKeyboardButton(f"{ability.name}{' ✅' if ability.activated else ''}",
+        button = InlineKeyboardButton(f"{'✅ ' if ability.activated else ''}{ability.name}",
                                       callback_data=f"ability_name|{ability.name}")
         row.append(button)
 
